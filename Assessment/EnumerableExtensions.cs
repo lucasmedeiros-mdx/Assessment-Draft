3c1ece71f5c38748c79a0f8a52d5ace67c08ab10
Lucas Medeiros <lucas@autoloop.com>
2018-07-03 09:02:35 -0300

    Adding Custom Where extension method to IEnumerable

---
+++ Assessment/EnumerableExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Assessment
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> CustomWhere<T>(this IEnumerable<T> item, Predicate<T> predicate)
+        {
+            return Enumerable.Empty<T>();
+        }
+    }
+}
---
+++ AssessmentTests/EnumerableExtensionTests.cs
@@ -0,0 +1,20 @@
+﻿using Assessment;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace AssessmentTests
+{
+    [TestClass]
+    public class EnumerableExtensionTests
+    {
+        [TestMethod]
+        public void CustomWhereShouldFilterItems()
+        {
+            var sequence = Enumerable.Range(0, 10);
+
+            var evenNumbers = sequence.CustomWhere(item => item % 2 == 0);
+
+            Assert.AreEqual(5, evenNumbers.Count());
+        }
+    }
+}
